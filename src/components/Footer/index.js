80bf8019babdc2c36c38b6af93e8b9267a4a9463
Leo Lima <leoglima@gmail.com>
2021-11-07 14:49:41 -0300

    começando a estrutura inicial do projeto

---
+++ src/components/Footer/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Footer (){
+    return (
+        <p>Base do site</p>
+    );
+}
+
+export default Footer;
---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Header (){
+    return (
+        <p>Topo do site</p>
+    );
+}
+
+export default Header;
